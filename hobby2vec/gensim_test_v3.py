4be6ede778e83a327703abd217816b16314b03ff
しゃーるう <1034534198@qq.com>
2017-10-16 22:07:07 +0800

    minor_update

--- hobby2vec/gensim_test_v3.py
+++ hobby2vec/gensim_test_v3.py
@@ -6,6 +6,8 @@ from TextSta_v2 import TextSta
 from gensim.corpora.dictionary import Dictionary
 import os
 import _pickle as pickle
+import jieba.posseg as psg
+
 folder_path = u"C:\\Users\\xiangrufan\\Desktop\\NLP\\Astro_NLP\\resource\\复旦分类语料\\answer"
 path_list = []
 for genre in os.listdir(folder_path):
@@ -29,13 +31,24 @@ for index in [""]+[str(i) for i in range(3,14)]:
         word_list = jieba.lcut(out)
         whole_list.extend(word_list)
 
+word_list = [tmp.word for tmp in whole_list]
+word2type_dict = {}
+for pair in whole_list:
+    if pair.word not in word2type_dict.keys():
+        word2type_dict[pair.word] = pair.flag
+    elif word2type_dict[pair.word] == pair.flag:
+        pass
+    else:
+        word2type_dict[pair.word] = pair.flag + ' ' + word2type_dict[pair.word]
+
 model = word2vec.Word2Vec()
-model.build_vocab([whole_list])
+model.sort_vocab()
+model.build_vocab([word_list])
 key_word_list = ['白羊','金牛','双子','巨蟹','狮子','处女','天秤','天蝎','射手','摩羯','水瓶','双鱼']
 for sign in key_word_list:
     try:
         pred_relation = model.similar_by_word(sign,3)
-        print ('the sign is most related with following word',[tmp[0] for tmp in pred_relation])
+        print ('the sign {:s}is most related with following word'.format(sign),[tmp[0] for tmp in pred_relation])
     except:
         print ('{:s} is not found'.format(sign))
         # TODO make this simple program be able to handle POS
